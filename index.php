813f607e6be09f6387153d3692a5fa01bdfd9d5a
Andy Suwito <andychin21@yahoo.com>
2012-01-08 15:39:54 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+	<meta name="author" content="andylah" />
+
+	<title>PT Sumber Makmur</title>
+</head>
+<!--<script type="text/javascript" src="javascript/function.js"></script>-->
+
+<link rel="stylesheet" type="text/css" href="images/style.css" />
+
+<body>
+
+<div id="container">
+    <div id="logo">
+        <div id="logo-depan">
+            <img src="images/logo-depan.jpg" alt="PT Sumber Makmur" width="520" height="344"/>
+        </div>
+    </div>
+    <div id="menu-utama">
+        <ul>
+            <li><a href="http://<?php echo $_SERVER['SERVER_NAME'] ?>/hasada/program/acc/index.php">Accounting Software</a></li>
+            <li><a href="#">Administration Software</a></li>
+            <li><a href="#">Nanny Catalog</a></li>
+        </ul>
+    </div>
+    <div id="footer">
+        <p>Copyright &copy PT Submer Makmur 2011, <a href="#">Nagoya Software House</a></p>
+    </div>
+</div>
+
+</body>
+</html> 
